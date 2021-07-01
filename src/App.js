cd7b5ca5427d5f679141321df956ea0d319a1d5e
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2021-07-01 17:58:37 +0200

    Refactoring App.js, Replacement of the stateful component by a functional component using Hooks & use useEffect with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Cities from "./features/cities";
 import Favorites from "./features/favorites";
@@ -10,48 +10,71 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
-export default class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      cities: null,
-      favorites: null,
-      loaded: false,
-    };
-  }
+const App = () => {
+  // https://www.robinwieruch.de/react-hooks-fetch-data => useEffect with Axios
+  const [cities, setCities] = useState([]);
+  useEffect(() => {
+    const fecthCities = () =>
+      apiCities
+        .get()
+        .then((res) => res.data._embedded["ua:item"])
+        .then((cities) => updateCities(sortCitiesByScore(cities)))
+        .catch((err) => console.log(err));
 
-  componentDidMount() {
-    apiCities
-      .get()
-      .then((res) => res.data._embedded["ua:item"])
-      .then((cities) => this.updateCities(this.sortCitiesByScore(cities)))
-      .catch((err) => console.log(err));
+    fecthCities();
+    console.log(cities);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    apiFirebase.get("favorites.json").then((res) => {
-      let favorites = res.data ? res.data : [];
-      this.updateFavorites(favorites);
-    });
-  }
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    const fetchFavorites = () =>
+      apiFirebase.get("favorites.json").then((res) => {
+        let favorites = res.data ? res.data : [];
+        updateFavorites(favorites);
+      });
+
+    fetchFavorites();
+    console.log(favorites);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [loaded, setLoaded] = useState(false);
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     cities: null,
+  //     favorites: null,
+  //     loaded: false,
+  //   };
+  // }
 
   // Méthode qui permet de modifier le state cities
-  updateCities = (cities) => {
-    this.setState({
-      cities,
-      loaded: this.state.favorites ? true : false,
-    });
+  const updateCities = (cities) => {
+    setCities(cities);
+    setLoaded(favorites ? true : false);
+
+    // this.setState({
+    //   cities,
+    //   loaded: this.state.favorites ? true : false,
+    // });
   };
 
   // Méthode qui permet de modifier le state favorites
-  updateFavorites = (favorites) => {
-    this.setState({
-      favorites,
-      loaded: this.state.cities ? true : false,
-    });
+  const updateFavorites = (favorites) => {
+    setFavorites(favorites);
+    setLoaded(cities ? true : false);
+
+    // this.setState({
+    //   favorites,
+    //   loaded: this.state.cities ? true : false,
+    // });
   };
 
   // Méthode qui permet de classer par ordre décroissant les villes en fonction de leurs scores générales
-  sortCitiesByScore = (cities) => {
+  const sortCitiesByScore = (cities) => {
     const sortByMapped = (map, compareFn) => (a, b) =>
       compareFn(map(a), map(b));
     const byValue = (a, b) => b - a;
@@ -61,90 +84,94 @@ export default class App extends Component {
   };
 
   // Méthode qui permet d'ajouter une ville en favoris
-  addFavorite = (id) => {
-    const favorites = [...this.state.favorites]; // Notation ES6 OU this.state.favorites.slice();
-    const city = this.state.cities.find((c) => c.ua_id === id);
-    favorites.push(city);
+  const addFavorite = (id) => {
+    const newFavorites = [...favorites]; // Notation ES6 OU this.state.favorites.slice();
+    const newCities = cities.find((c) => c.ua_id === id);
+    newFavorites.push(newCities);
 
-    this.setState(
-      {
-        favorites,
-      },
-      () => {
-        this.saveFavorites();
-      }
-    );
+    setFavorites(newFavorites);
+
+    // this.setState(
+    //   {
+    //     favorites,
+    //   },
+    //   () => {
+    //     saveFavorites();
+    //   }
+    // );
   };
 
   // Méthode qui permet de supprimer une ville des favoris
-  removeFavorite = (id) => {
-    const favorites = [...this.state.favorites]; // Notation ES6 OU this.state.favorites.slice()
-    const index = this.state.favorites.findIndex((f) => f.ua_id === id);
-    favorites.splice(index, 1);
+  const removeFavorite = (id) => {
+    const newFavorites = [...favorites]; // Notation ES6 OU this.state.favorites.slice()
+    const index = favorites.findIndex((f) => f.ua_id === id);
+    newFavorites.splice(index, 1);
 
-    this.setState(
-      {
-        favorites,
-      },
-      () => {
-        this.saveFavorites();
-      }
-    );
+    setFavorites(newFavorites);
+
+    // this.setState(
+    //   {
+    //     favorites,
+    //   },
+    //   () => {
+    //     this.saveFavorites();
+    //   }
+    // );
   };
 
-  saveFavorites = () => {
-    apiFirebase.put("favorites.json", this.state.favorites);
-  };
+  // const saveFavorites = () => {
+  //   apiFirebase.put("favorites.json", favorites);
+  // };
 
   // Méthode qui permet de supprimer une ville de la liste des villes
-  removeCity = (id) => {
-    const cities = [...this.state.cities]; // 1 => Copie une référence du tableau original
-    const index = this.state.cities.findIndex((c) => c.ua_id === id); // 2 => Récupère l'index de l'élément du tableau à retirer en vérifiant leurs index
-    cities.splice(index, 1); // 3 => Modifie le contenu de mon tableau en retirant un élément grâce à l'index
+  const removeCity = (id) => {
+    const newCities = [...cities]; // 1 => Copie une référence du tableau original
+    const index = cities.findIndex((c) => c.ua_id === id); // 2 => Récupère l'index de l'élément du tableau à retirer en vérifiant leurs index
+    newCities.splice(index, 1); // 3 => Modifie le contenu de mon tableau en retirant un élément grâce à l'index
 
     // 4 => Modifie mon state en remplaçant l'ancien tableau par le niveau
-    this.setState({
-      cities,
-    });
+    setCities(newCities);
+    // this.setState({
+    //   cities,
+    // });
   };
 
-  render() {
-    console.log(this.state.cities);
-    return (
-      <Router>
-        <Switch>
-          <Route
-            path="/cities"
-            render={(props) => {
-              return (
-                <Cities
-                  {...props}
-                  loaded={this.state.loaded}
-                  cities={this.state.cities}
-                  favorites={this.state.favorites}
-                  addFavorite={this.addFavorite}
-                  removeFavorite={this.removeFavorite}
-                  removeCity={this.removeCity}
-                />
-              );
-            }}
-          />
-          <Route
-            path="/favorites"
-            render={(props) => {
-              return (
-                <Favorites
-                  {...props}
-                  loaded={this.state.loaded}
-                  favorites={this.state.favorites}
-                  removeFavorite={this.removeFavorite}
-                />
-              );
-            }}
-          />
-          <Redirect to="/cities" />
-        </Switch>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path="/cities"
+          render={(props) => {
+            return (
+              <Cities
+                {...props}
+                loaded={loaded}
+                cities={cities}
+                favorites={favorites}
+                addFavorite={addFavorite}
+                removeFavorite={removeFavorite}
+                removeCity={removeCity}
+              />
+            );
+          }}
+        />
+        <Route
+          path="/favorites"
+          render={(props) => {
+            return (
+              <Favorites
+                {...props}
+                loaded={loaded}
+                favorites={favorites}
+                removeFavorite={removeFavorite}
+              />
+            );
+          }}
+        />
+        <Redirect to="/cities" />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
