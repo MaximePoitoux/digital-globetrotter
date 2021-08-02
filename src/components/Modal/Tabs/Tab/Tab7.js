2e262af447467ebdd1edb8fda3c8d608d40b91b9
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2021-08-02 19:34:14 +0200

    Digital Globetrotter guide tab in development

--- src/components/Modal/Tabs/Tab/Tab7.js
+++ src/components/Modal/Tabs/Tab/Tab7.js
@@ -1,6 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { BiWorld } from "react-icons/bi";
 import { IoFlag } from "react-icons/io5";
+import { IoIosPeople } from "react-icons/io";
+import { FaWifi } from "react-icons/fa";
+import { FaSmog } from "react-icons/fa";
+import { HiSpeakerphone } from "react-icons/hi";
+import { HiCurrencyDollar } from "react-icons/hi";
+import { RiExchangeDollarFill } from "react-icons/ri";
+import { BsPeopleFill } from "react-icons/bs";
 
 const useStyles = makeStyles(() => ({
   climateContainer: {
@@ -37,6 +44,99 @@ const Tab7 = ({ city }) => {
     return city.full_name.split(",")[1];
   };
 
+  const getPopulationSize = (city) => {
+    const millionsFormate = (value) => {
+      const nf = new Intl.NumberFormat("en-IN", { minimumFractionDigits: 6 });
+      return nf.format(value);
+    };
+
+    const numberWithSpaces = (value) => {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    };
+
+    return city._embedded["ua:details"].categories
+      .filter((category) => category.id === "CITY-SIZE")
+      .map((citySize) =>
+        citySize.data
+          .filter((x) => x.id === "POPULATION-SIZE")
+          .map((population) =>
+            numberWithSpaces(
+              millionsFormate(population.float_value)
+                .split(".")
+                .join(" ")
+                .replace(/^0+/, "")
+            )
+          )
+      );
+  };
+
+  const getInternetSpeed = (city) => {
+    return city._embedded["ua:details"].categories
+      .filter((x) => x.id === "NETWORK")
+      .map((x) => Math.round(x.data[0].float_value));
+  };
+
+  const getAirQuality = (city) => {
+    return city._embedded["ua:details"].categories
+      .filter((category) => category.id === "POLLUTION")
+      .map((pollution) =>
+        pollution.data
+          .filter((p) => p.id === "AIR-POLLUTION-TELESCORE")
+          .map((air) => Math.round(air.float_value * 100) + " %")
+      );
+  };
+
+  const getSpokenLanguages = (city) => {
+    return city._embedded["ua:details"].categories
+      .filter((category) => category.id === "LANGUAGE")
+      .map((language) =>
+        language.data
+          .filter((l) => l.id === "SPOKEN-LANGUAGES")
+          .map((languages) => languages.string_value)
+      );
+  };
+
+  const getCurrency = (city) => {
+    return city._embedded["ua:details"].categories
+      .filter((category) => category.id === "ECONOMY")
+      .map((economy) =>
+        economy.data
+          .filter((e) => e.id === "CURRENCY-URBAN-AREA")
+          .map((currency) => currency.string_value)
+      );
+  };
+
+  const getCurrencyExchangeRate = (city) => {
+    const rounded2DigitsComma = (number) => {
+      const num = Number(number); // The Number() only visualizes the type and is not needed
+      const roundedString = num.toFixed(1);
+      const rounded = Number(roundedString);
+
+      return rounded;
+    };
+
+    return city._embedded["ua:details"].categories
+      .filter((category) => category.id === "ECONOMY")
+      .map((economy) =>
+        economy.data
+          .filter((e) => e.id === "CURRENCY-URBAN-AREA-EXCHANGE-RATE")
+          .map((currency) => rounded2DigitsComma(currency.float_value))
+      );
+  };
+
+  const getGdpPerCapita = (city) => {
+    return city._embedded["ua:details"].categories
+      .filter((category) => category.id === "ECONOMY")
+      .map((economy) =>
+        economy.data
+          .filter((e) => e.id === "GDP-PER-CAPITA")
+          .map(
+            (gdp) =>
+              Math.round(gdp.currency_dollar_value).toLocaleString() + " $"
+          )
+      );
+  };
+
   return (
     <div className={classes.climateContainer}>
       <div
@@ -63,6 +163,101 @@ const Tab7 = ({ city }) => {
         />
         Country
         <div className={classes.valueContainer}>{getCountry(city)}</div>
+      </div>
+      <div
+        className={classes.elementContainer}
+        style={{ background: "#ecf0f1" }}
+      >
+        <IoIosPeople
+          style={{
+            marginRight: "10px",
+            fontSize: "1.4em",
+            minWidth: "40px",
+          }}
+        />
+        Population
+        <div className={classes.valueContainer}>
+          {getPopulationSize(city)} people
+        </div>
+      </div>
+      <div className={classes.elementContainer} style={{ background: "white" }}>
+        <FaWifi
+          style={{
+            marginRight: "10px",
+            fontSize: "1.1em",
+            minWidth: "40px",
+          }}
+        />
+        Internet speed
+        <div className={classes.valueContainer}>
+          {getInternetSpeed(city)} Mbps
+        </div>
+      </div>
+      <div
+        className={classes.elementContainer}
+        style={{ background: "#ecf0f1" }}
+      >
+        <FaSmog
+          style={{
+            marginRight: "10px",
+            fontSize: "1.1em",
+            minWidth: "40px",
+          }}
+        />
+        Air quality
+        <div className={classes.valueContainer}>{getAirQuality(city)}</div>
+      </div>
+      <div className={classes.elementContainer} style={{ background: "white" }}>
+        <HiSpeakerphone
+          style={{
+            marginRight: "10px",
+            fontSize: "1.4em",
+            minWidth: "40px",
+          }}
+        />
+        Spoken languages
+        <div className={classes.valueContainer}>{getSpokenLanguages(city)}</div>
+      </div>
+      <div
+        className={classes.elementContainer}
+        style={{ background: "#ecf0f1" }}
+      >
+        <HiCurrencyDollar
+          style={{
+            marginRight: "10px",
+            fontSize: "1.4em",
+            minWidth: "40px",
+          }}
+        />
+        Currency
+        <div className={classes.valueContainer}>{getCurrency(city)}</div>
+      </div>
+      <div className={classes.elementContainer} style={{ background: "white" }}>
+        <RiExchangeDollarFill
+          style={{
+            marginRight: "10px",
+            fontSize: "1.4em",
+            minWidth: "40px",
+          }}
+        />
+        Currency exchange rate per US dollar for urban area
+        <div className={classes.valueContainer}>
+          {getCurrencyExchangeRate(city)} {getCurrency(city)}
+        </div>
+      </div>
+      <div
+        className={classes.elementContainer}
+        style={{ background: "#ecf0f1" }}
+      >
+        <BsPeopleFill
+          style={{
+            marginRight: "10px",
+            fontSize: "1.4em",
+            minWidth: "40px",
+          }}
+        />
+        GDP per capita
+        <div className={classes.valueContainer}>{getGdpPerCapita(city)}</div>
       </div>
     </div>
   );
