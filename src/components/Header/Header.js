738549a38cabd641d99c1f0c064a4ccb84e128a6
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2021-08-05 15:49:02 +0200

    Modification of the header and addition of an intro video

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import { AiFillHome, AiFillHeart } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
+import video from "../../assets/videos/intro.mp4";
 
 const useStyles = makeStyles((theme) => ({
   headerContainer: {
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    padding: "10px 60px 10px 0px",
+    position: "relative",
+    // display: "flex",
+    // justifyContent: "flex-end",
+    // alignItems: "center",
+    padding: "0px 0px 50px 0px",
   },
   link: {
     display: "flex",
@@ -18,6 +20,20 @@ const useStyles = makeStyles((theme) => ({
     color: "#b2bec3",
     padding: 5,
   },
+  logoContainer: {
+    padding: "0px 20px",
+    position: "absolute",
+    top: "0",
+    left: "0",
+    display: "flex",
+    // justifyContent: "center",
+    alignItems: "center",
+    // flexDirection: "column",
+    // alignItems: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "1.7em",
+  },
 }));
 
 const Header = () => {
@@ -25,14 +41,14 @@ const Header = () => {
 
   return (
     <div className={classes.headerContainer}>
-      <img
+      {/* <img
         src={logo}
         alt="logo"
         style={{
           height: "200px",
           width: "auto",
         }}
-      />{" "}
+      />
       Digital Globetrotter
       <NavLink
         className={classes.link}
@@ -55,7 +71,34 @@ const Header = () => {
       >
         <AiFillHeart style={{ marginRight: 4 }} />
         Favorites
-      </NavLink>
+      </NavLink> */}
+      <video
+        style={{
+          objectFit: "cover",
+          zIndex: "-100",
+        }}
+        width="100%"
+        height="500px"
+        src={video}
+        // controls="controls"
+        autoplay="true"
+        muted
+        loop
+      />
+      <div className={classes.logoContainer}>
+        <img
+          src={logo}
+          alt="logo"
+          style={{
+            height: "60px",
+            width: "auto",
+            filter: "drop-shadow(0 0 0.5rem black)",
+          }}
+        />
+        <p style={{ marginLeft: "20px", letterSpacing: "0.06em" }}>
+          Digital<br></br>Globetrotter
+        </p>
+      </div>
     </div>
   );
 };
