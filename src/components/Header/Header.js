b2dbfe9fee1f442b1835babce57d6e582139de38
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2021-08-11 19:12:15 +0200

    Fix 10 intro video

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { AiFillHome, AiFillHeart } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-// import poster from "../../assets/images/poster.jpg";
+import poster from "../../assets/images/poster.jpg";
 import video from "../../assets/videos/intro2.mp4";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -96,6 +96,7 @@ const Header = ({
           autoPlay
           muted
           loop
+          poster={poster}
         />
       </LazyLoad>
       <div
