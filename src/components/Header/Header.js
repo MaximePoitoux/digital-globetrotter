0dd99bcc54dce9aa70021b706dc03d258e244b12
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2021-08-11 18:12:09 +0200

    Fixv6 intro video

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,39 +11,6 @@ import Searchbar from "../Searchbar/Searchbar";
 
 // https://medium.com/@BoltAssaults/autoplay-muted-html5-video-safari-ios-10-in-react-673ae50ba1f5
 
-document.addEventListener("DOMContentLoaded", function () {
-  var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
-
-  if ("IntersectionObserver" in window) {
-    var lazyVideoObserver = new IntersectionObserver(function (
-      entries,
-      observer
-    ) {
-      entries.forEach(function (video) {
-        if (video.isIntersecting) {
-          for (var source in video.target.children) {
-            var videoSource = video.target.children[source];
-            if (
-              typeof videoSource.tagName === "string" &&
-              videoSource.tagName === "SOURCE"
-            ) {
-              videoSource.src = videoSource.dataset.src;
-            }
-          }
-
-          video.target.load();
-          video.target.classList.remove("lazy");
-          lazyVideoObserver.unobserve(video.target);
-        }
-      });
-    });
-
-    lazyVideos.forEach(function (lazyVideo) {
-      lazyVideoObserver.observe(lazyVideo);
-    });
-  }
-});
-
 const useStyles = makeStyles((theme) => ({
   headerContainer: {
     position: "relative",
@@ -122,8 +89,8 @@ const Header = ({
         }}
         width="100%"
         height="500px"
+        poster="placeholder.png"
         src={video}
-        // controls="controls"
         playsInline
         autoPlay
         muted
